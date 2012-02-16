403295c0169a8a69a0bb43fdc2557b94257840fc
René Sato <sato-san@impresscms.org>
2012-02-16 02:20:46 +0000

    folderstructure changed
    git-svn-id: https://subversion.assembla.com/svn/impresscmsaddons/modules/cms/trunk@23573 7a4d4d67-0c3a-0410-b24c-89d50c169223

---
+++ archiviert_start.php
@@ -0,0 +1,411 @@
+<?php
+/**
+ * Start index page - displays details of a single start, a list of start summary descriptions or a compact table of cms
+ *
+ * @copyright	Copyright Madfish (Simon Wilkinson) 2012
+ * @license		http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU General Public License (GPL)
+ * @since		1.0
+ * @author		Madfish (Simon Wilkinson) <simon@isengard.biz>
+ * @package		cms
+ * @version		$sato-san$
+*/
+
+include_once "header.php";
+
+$xoopsOption["template_main"] = "cms_start.html";
+include_once ICMS_ROOT_PATH . "/header.php";
+
+// Sanitise input parameters
+$clean_start_id = isset($_GET["start_id"]) ? (int)$_GET["start_id"] : 0 ;
+$clean_tag_id = isset($_GET["tag_id"]) ? (int)$_GET["tag_id"] : 0 ;
+$clean_start = isset($_GET["start"]) ? (int)($_GET["start"]) : 0;
+
+// Get the requested start, or retrieve the index page. Only show online cms
+$cms_start_handler = icms_getModuleHandler("start", basename(dirname(__FILE__)), "cms");
+$criteria = icms_buildCriteria(array('online_status' => '1'));
+$startObj = $cms_start_handler->get($clean_start_id, TRUE, FALSE, $criteria);
+
+// Get relative path to document root for this ICMS install. This is required to call the logos correctly if ICMS is installed in a subdirectory
+$directory_name = basename(dirname(__FILE__));
+$script_name = getenv("SCRIPT_NAME");
+$document_root = str_replace('modules/' . $directory_name . '/archiviert_start.php', '', $script_name);
+
+// Optional tagging support (only if Sprockets module installed)
+$sprocketsModule = icms::handler("icms_module")->getByDirname("sprockets");
+$cms_tag_name = '';
+
+if (icms_get_module_status("sprockets"))
+{
+	icms_loadLanguageFile("sprockets", "common");
+	$sprockets_tag_handler = icms_getModuleHandler('tag', $sprocketsModule->getVar('dirname'), 'sprockets');
+	$sprockets_taglink_handler = icms_getModuleHandler('taglink', $sprocketsModule->getVar('dirname'), 'sprockets');
+	$sprockets_tag_buffer = $sprockets_tag_handler->getObjects(NULL, TRUE, FALSE);
+}
+
+// Assign common logo preferences to template
+$icmsTpl->assign('display_start_logos', icms::$module->config['display_start_logos']);
+$icmsTpl->assign('freestyle_logo_dimensions', icms::$module->config['freestyle_logo_dimensions']);
+$icmsTpl->assign('logo_display_width', icms::$module->config['logo_index_display_width']);
+if (icms::$module->config['start_logo_position'] == 1) // Align right
+{
+	$icmsTpl->assign('start_logo_position', 'cms_float_right');
+}
+else // Align left
+{
+	$icmsTpl->assign('start_logo_position', 'cms_float_left');
+}
+
+/////////////////////////////////////////
+////////// VIEW SINGLE START //////////
+/////////////////////////////////////////
+
+if($startObj && !$startObj->isNew())
+{	
+	//comments detailpage
+	if ($cmsConfig['com_rule']) {
+		$icmsTpl->assign('cms_post_comment', TRUE);
+		include_once ICMS_ROOT_PATH . '/include/comment_view.php';
+	}
+	
+	// Update hit counter
+	if (!icms_userIsAdmin(icms::$module->getVar('dirname')))
+	{
+		$cms_start_handler->updateCounter($startObj);
+	}
+	
+	// Convert object to array for easy insertion into templates
+	$start = $startObj->toArray();
+	
+	// Check if the hit counter should be displayed or not
+	if (icms::$module->config['show_view_counter'] == FALSE)
+	{
+		unset($start['counter']);
+	}
+
+	// Adjust logo path for template
+	if (!empty($start['logo']))
+	{
+		$start['logo'] = $document_root . 'uploads/' . $directory_name . '/start/' . $start['logo'];
+	}
+	
+	// Prepare tags for display
+	if (icms_get_module_status("sprockets"))
+	{
+		$start['tags'] = array();
+		$start_tag_array = $sprockets_taglink_handler->getTagsForObject($startObj->getVar('start_id'), $cms_start_handler);
+		foreach ($start_tag_array as $key => $value)
+		{
+			$start['tags'][$value] = '<a href="' . CMS_URL . 'archiviert_start.php?tag_id=' . $value 
+					. '" title="' . _CO_CMS_TAGS_ALL_CONTENTS_ON . ' '. $sprockets_tag_buffer[$value]['title']
+						.' ' . _CO_CMS_TAGS_ALL_SHOW . '">' . $sprockets_tag_buffer[$value]['title'] . '</a>';
+		}
+		$start['tags'] = implode(' ', $start['tags']);
+	}
+
+	// If the start is archiviert, add the archiviert flag to the breadcrumb title
+	if ($startObj->getVar('beendet', 'e') == 1)
+	{
+		$icmsTpl->assign("cms_archiviert_path", _CO_CMS_START_BEENDET);
+	}
+	
+	$icmsTpl->assign("cms_start", $start);
+
+	$icms_metagen = new icms_ipf_Metagen($startObj->getVar("title"), 
+			$startObj->getVar("meta_keywords", "n"), $startObj->getVar("meta_description", "n"));
+	$icms_metagen->createMetaTags();
+	$icmsTpl->assign('logo_display_width', icms::$module->config['logo_single_display_width']);
+}
+
+////////////////////////////////////////
+////////// VIEW START INDEX //////////
+////////////////////////////////////////
+else
+{	
+	// Get a select box (if preferences allow, and only if Sprockets module installed)
+	if (icms_get_module_status("sprockets") && icms::$module->config['show_tag_select_box'] == TRUE)
+	{
+		// Initialise
+		$cms_tag_name = '';
+		$tagList = array();
+		$sprockets_tag_handler = icms_getModuleHandler('tag', $sprocketsModule->getVar('dirname'),
+				'sprockets');
+		$sprockets_taglink_handler = icms_getModuleHandler('taglink', 
+				$sprocketsModule->getVar('dirname'), 'sprockets');
+
+		// Append the tag to the breadcrumb title
+		if (array_key_exists($clean_tag_id, $sprockets_tag_buffer) && ($clean_tag_id !== 0))
+		{
+			$cms_tag_name = $sprockets_tag_buffer[$clean_tag_id]['title'];
+			$icmsTpl->assign('cms_category_path', $sprockets_tag_buffer[$clean_tag_id]['title']);
+		}
+		
+		// Load the tag navigation select box
+		// $action, $selected = null, $zero_option_message = '---', 
+		// $navigation_elements_only = TRUE, $module_id = null, $item = null,
+		$tag_select_box = $sprockets_tag_handler->getTagSelectBox('archiviert_start.php', $clean_tag_id, 
+				_CO_CMS_START_ALL_TAGS, TRUE, icms::$module->getVar('mid'));
+		$icmsTpl->assign('cms_tag_select_box', $tag_select_box);
+	}
+	
+	///////////////////////////////////////////////////////////////////
+	////////// View cms as list of summary descriptions //////////
+	///////////////////////////////////////////////////////////////////
+	if (icms::$module->config['index_display_mode'] == TRUE)
+	{
+		$start_summaries = $linked_start_ids = array();
+		
+		// Append the tag name to the module title (if preferences allow, and only if Sprockets module installed)
+		if (icms_get_module_status("sprockets") && icms::$module->config['show_breadcrumb'] == FALSE)
+		{
+			if (array_key_exists($clean_tag_id, $sprockets_tag_buffer) && ($clean_tag_id !== 0))
+			{
+				$cms_tag_name = $sprockets_tag_buffer[$clean_tag_id]['title'];
+				$icmsTpl->assign('cms_tag_name', $cms_tag_name);
+			}
+		}
+				
+		// Retrieve cms for a given tag
+		if ($clean_tag_id && icms_get_module_status("sprockets"))
+		{
+			/**
+			 * Retrieve a list of cms JOINED to taglinks by start_id/tag_id/module_id/item
+			 */
+
+			$query = $rows = $start_count = '';
+			$linked_start_ids = array();
+			
+			// First, count the number of cms for the pagination control
+			$start_count = '';
+			$group_query = "SELECT count(*) FROM " . $cms_start_handler->table . ", "
+					. $sprockets_taglink_handler->table
+					. " WHERE `start_id` = `iid`"
+					. " AND `online_status` = '1'"
+					. " AND `beendet` = '1'"
+					. " AND `tid` = '" . $clean_tag_id . "'"
+					. " AND `mid` = '" . icms::$module->getVar('mid') . "'"
+					. " AND `item` = 'start'";
+			
+			$result = icms::$xoopsDB->query($group_query);
+
+			if (!$result)
+			{
+				echo 'Error';
+				exit;	
+			}
+			else
+			{
+				while ($row = icms::$xoopsDB->fetchArray($result))
+				{
+					foreach ($row as $key => $count) 
+					{
+						$start_count = $count;
+					}
+				}
+			}
+			
+			// Secondly, get the cms
+			$query = "SELECT * FROM " . $cms_start_handler->table . ", "
+					. $sprockets_taglink_handler->table
+					. " WHERE `start_id` = `iid`"
+					. " AND `online_status` = '1'"
+					. " AND `beendet` = '1'"
+					. " AND `tid` = '" . $clean_tag_id . "'"
+					. " AND `mid` = '" . icms::$module->getVar('mid') . "'"
+					. " AND `item` = 'start'"
+					. " ORDER BY `date` DESC" //geändert von weight ASC zu date DESC
+					. " LIMIT " . $clean_start . ", " . icms::$module->config['number_of_cms_per_page'];
+
+			$result = icms::$xoopsDB->query($query);
+
+			if (!$result)
+			{
+				echo 'Error';
+				exit;
+			}
+			else
+			{
+				$rows = $cms_start_handler->convertResultSet($result, TRUE, FALSE);
+				foreach ($rows as $key => $row) 
+				{
+					$start_summaries[$row['start_id']] = $row;
+				}
+			}
+		}
+				
+		// Retrieve cms without filtering by tag
+		else
+		{
+			$criteria = new icms_db_criteria_Compo();
+			$criteria->add(new icms_db_criteria_Item('beendet', '1'));
+			$criteria->add(new icms_db_criteria_Item('online_status', '1'));
+
+			// Count the number of online cms for the pagination control
+			$start_count = $cms_start_handler->getCount($criteria);
+
+			// Continue to retrieve cms for this page view
+			$criteria->setStart($clean_start);
+			$criteria->setLimit(icms::$module->config['number_of_cms_per_page']);
+			$criteria->setSort('date');
+			$criteria->setOrder('DESC');
+			$start_summaries = $cms_start_handler->getObjects($criteria, TRUE, FALSE);
+		}
+		
+		// Prepare tags. A list of start IDs is used to retrieve relevant taglinks. The taglinks
+		// are sorted into a multidimensional array, using the start ID as the key to each subarray.
+		// Then its just a case of assigning each subarray to the matching start.
+		 
+		// Prepare a list of start_id, this will be used to create a taglink buffer, which is used
+		// to create tag links for each start
+		$linked_start_ids = '';
+		foreach ($start_summaries as $key => $value) {
+			$linked_start_ids[] = $value['start_id'];
+		}
+		
+		if (icms_get_module_status("sprockets") && !empty($linked_start_ids))
+		{
+			$linked_start_ids = '(' . implode(',', $linked_start_ids) . ')';
+
+			// Prepare multidimensional array of tag_ids with start_id (iid) as key
+			$taglink_buffer = $start_tag_id_buffer = array();
+			$criteria = new  icms_db_criteria_Compo();
+			$criteria->add(new icms_db_criteria_Item('mid', icms::$module->getVar('mid')));
+			$criteria->add(new icms_db_criteria_Item('item', 'start'));
+			$criteria->add(new icms_db_criteria_Item('iid', $linked_start_ids, 'IN'));
+			$taglink_buffer = $sprockets_taglink_handler->getObjects($criteria, TRUE, TRUE);
+			unset($criteria);
+
+			// Build tags, with URLs for navigation
+			foreach ($taglink_buffer as $key => $taglink) {
+
+				if (!array_key_exists($taglink->getVar('iid'), $start_tag_id_buffer)) {
+					$start_tag_id_buffer[$taglink->getVar('iid')] = array();
+				}
+				$start_tag_id_buffer[$taglink->getVar('iid')][] = '<a href="' . CMS_URL . 
+						'archiviert_start.php?tag_id=' . $taglink->getVar('tid') . '" title="' . _CO_CMS_TAGS_ALL_CONTENTS_ON . ' '. $sprockets_tag_buffer[$taglink->getVar('tid')]['title']
+						.' ' . _CO_CMS_TAGS_ALL_SHOW . '">' 
+						. $sprockets_tag_buffer[$taglink->getVar('tid')]['title']
+						. '</a>';
+			}
+
+			// Convert the tag arrays into strings for easy handling in the template
+			foreach ($start_tag_id_buffer as $key => &$value) 
+			{
+				$value = implode(' ', $value);
+			}
+
+			// Assign each subarray of tags to the matching cms, using the item id as marker
+			foreach ($start_summaries as $key => &$value) {
+				if (!empty($start_tag_id_buffer[$value['start_id']]))
+				{
+					$value['tags'] = $start_tag_id_buffer[$value['start_id']];
+				}
+			}
+		}
+		
+		// Prepare start for display
+		foreach ($start_summaries as &$start)
+		{			
+			// Ajust logo paths to allow dynamic image resizing
+			if (!empty($start['logo']))
+			$start['logo'] = $document_root . 'uploads/' . $directory_name . '/start/'
+				. $start['logo'];
+			
+			// Alter the itemUrl to point at the archiviert cms page, rather than the cms page
+			$start['itemUrl'] = str_replace('start.php', 'archiviert_start.php', $start['itemUrl']);
+		}
+		$icmsTpl->assign('start_summaries', $start_summaries);
+		
+		// Adjust pagination for tag, if present
+		if (!empty($clean_tag_id))
+		{
+			$extra_arg = 'tag_id=' . $clean_tag_id;
+		}
+		else
+		{
+			$extra_arg = TRUE;
+		}
+		
+		// Pagination control
+		$pagenav = new icms_view_PageNav($start_count, icms::$module->config['number_of_cms_per_page'],
+				$clean_start, 'start', $extra_arg);
+		$icmsTpl->assign('cms_navbar', $pagenav->renderNav());
+	}
+	else 
+	{
+		//////////////////////////////////////////////////////////////////////////////
+		////////// View cms in compact table, optionally filter by tag //////////
+		//////////////////////////////////////////////////////////////////////////////
+		
+		$tagged_start_list = '';
+		
+		if ($clean_tag_id && icms_get_module_status("sprockets")) 
+		{
+			// Get a list of start IDs belonging to this tag
+			$criteria = new icms_db_criteria_Compo();
+			$criteria->add(new icms_db_criteria_Item('tid', $clean_tag_id));
+			$criteria->add(new icms_db_criteria_Item('mid', icms::$module->getVar('mid')));
+			$criteria->add(new icms_db_criteria_Item('item', 'start'));
+			$criteria->setSort('date');
+			$criteria->setOrder('DESC');
+			$taglink_array = $sprockets_taglink_handler->getObjects($criteria);
+			foreach ($taglink_array as $taglink) {
+				$tagged_start_list[] = $taglink->getVar('iid');
+			}
+			$tagged_start_list = "('" . implode("','", $tagged_start_list) . "')";
+			unset($criteria);			
+		}
+		$criteria = new icms_db_criteria_Compo();
+		if (!empty($tagged_start_list))
+		{
+			$criteria->add(new icms_db_criteria_Item('start_id', $tagged_start_list, 'IN'));
+		}
+		$criteria->add(new icms_db_criteria_Item('beendet', '1'));
+		$criteria->add(new icms_db_criteria_Item('online_status', '1'));
+		$criteria->setSort('date');
+		$criteria->setOrder('DESC');
+		
+		// Retrieve the table
+		$objectTable = new icms_ipf_view_Table($cms_start_handler, $criteria, array());
+		$objectTable->isForUserSide();
+		$objectTable->addQuickSearch(array('title','description','extended_text'));
+		$objectTable->addColumn(new icms_ipf_view_Column("title", "left", "100", 'getItemLink'));
+		$objectTable->addColumn(new icms_ipf_view_Column("date", "center", "20"));
+		$objectTable->addColumn(new icms_ipf_view_Column("last_update", "center", "20"));
+		$objectTable->addColumn(new icms_ipf_view_Column("counter", "center", "20"));
+		$objectTable->setDefaultSort('date');
+		$objectTable->setDefaultOrder('DESC');
+		$icmsTpl->assign("cms_start_table", $objectTable->fetch());
+	}
+	
+	//comments index page
+	if ($cmsConfig['com_rule']) {
+		$icmsTpl->assign('cms_post_comment', TRUE);
+		include_once ICMS_ROOT_PATH . '/include/comment_view.php';
+	}
+}
+
+// Breadcrumb	
+if (icms::$module->config['show_breadcrumb'])
+{
+	$icmsTpl->assign("show_breadcrumb", icms::$module->config['show_breadcrumb']);
+	$icmsTpl->assign("cms_module_home", '<a href="' . ICMS_URL . "/modules/" 
+			. icms::$module->getVar("dirname") . '/">' . icms::$module->getVar("name") . "</a>");
+	if ($cms_tag_name)
+	{
+		$icmsTpl->assign("cms_archiviert_path", '<a href="' . ICMS_URL . "/modules/"
+		. icms::$module->getVar("dirname") . '/archiviert_start.php">'
+		. _CO_CMS_START_BEENDET . "</a>");
+	}
+	else
+	{
+		$icmsTpl->assign("cms_archiviert_path", '<a href="' . ICMS_URL . "/modules/"
+		. icms::$module->getVar("dirname") . '/archiviert_start.php">'
+		. _CO_CMS_START_BEENDET . "</a>");
+	}
+	
+}
+
+// Page title
+$icmsTpl->assign("cms_page_title", _CO_CMS_BEENDET_CMS);
+
+include_once "footer.php";
---
+++ icms_version.php
@@ -0,0 +1,278 @@
+<?php
+/**
+ * cms version infomation
+ *
+ * This file holds the configuration information of this module
+ *
+ * @copyright	Copyright Madfish (Simon Wilkinson) 2012
+ * @license		http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU General Public License (GPL)
+ * @since		1.0
+ * @author		Madfish (Simon Wilkinson) <simon@isengard.biz>
+ * @package		cms
+ * @version		$sato-san$
+*/
+
+defined("ICMS_ROOT_PATH") or die("ICMS root path not defined");
+
+/**  General Information  */
+$modversion = array(
+	"name"						=> _MI_CMS_MD_NAME,
+	"version"					=> 1.0,
+	"description"				=> _MI_CMS_MD_DESC,
+	"author"					=> "Madfish (Simon Wilkinson)",
+	"credits"					=> "Thanks to QM-B and Lotus for alls tests.",
+	"help"						=> "",
+	"license"					=> "GNU General Public License (GPL)",
+	"official"					=> 0,
+	"dirname"					=> basename(dirname(__FILE__)),
+	"modname"					=> "cms",
+
+/**  Images information  */
+	"iconsmall"					=> "images/icon_small.png",
+	"iconbig"					=> "images/icon_big.png",
+	"image"						=> "images/icon_big.png", /* for backward compatibility */
+
+/**  Development information */
+	"status_version"			=> "1.0",
+	"status"					=> "RC 4",
+	"date"						=> "13.02.2012",
+	"author_word"				=> "For ICMS 1.3+ only.",
+	"warning"					=> "Release candidate",
+
+/** Contributors */
+	"developer_website_url"		=> "",
+	"developer_website_name"	=> "",
+	"developer_email"			=> "",
+
+/** Administrative information */
+	"hasAdmin"					=> 1,
+	"adminindex"				=> "admin/index.php",
+	"adminmenu"					=> "admin/menu.php",
+
+/** Install and update informations */
+	"onInstall"					=> "include/onupdate.inc.php",
+	"onUpdate"					=> "include/onupdate.inc.php",
+
+/** Search information */
+	"hasSearch"					=> 1,
+	"search"					=> array("file" => "include/search.inc.php", "func" => "cms_search"),
+
+/** Comments information */
+	"hasComments"				=> 1,
+	"comments"					=> array(
+									"itemName" => "start_id",
+									"pageName" => "start.php",
+									"callbackFile" => "include/comment.inc.php",
+									"callback" => array("approve" => "cms_com_approve",
+									"update" => "cms_com_update")));
+
+/** Menu information */
+$modversion["hasMain"] = 1;
+$modversion["sub"][0]["name"] = _MI_CMS_ARCHIVIERT;
+$modversion["sub"][0]["url"] = "archiviert_start.php";
+
+/** other possible types: testers, translators, documenters and other */
+$modversion['people']['developers'][] = "<a href='http://community.impresscms.org/userinfo.php?uid=91' target='_blank'>Madfish</a> | Simon Wilkinson | <a href='https://www.isengard.biz'>isengard.biz</a> | simon@isengard.biz";
+$modversion['people']['developers'][] = "<a href='http://community.impresscms.org/userinfo.php?uid=10' target='_blank'>sato-san</a> | René Sato| 佐藤レネー | <a href='http://www.impresscms.de'>impresscms.de</a> | sato-san@impresscms.org";
+
+/** Manual */
+$modversion['manual']['wiki'][] = "<a href='http://wiki.impresscms.org/index.php?title=cms' target='_blank'>English</a>";
+
+/** Database information */
+$modversion['object_items'][1] = 'start';
+
+$modversion["tables"] = icms_getTablesArray($modversion['dirname'], $modversion['object_items']);
+
+/** Templates information */
+$modversion['templates'] = array(
+	array("file" => "cms_admin_start.html", "description" => "Start admin index."),
+	array("file" => "cms_start.html", "description" => "Start index."),
+	array("file" => "cms_header.html", "description" => "Module header."),
+	array("file" => "cms_footer.html", "description" => "Module footer."),
+	array("file" => "cms_requirements.html", "description" => "Alert if module requirements not met."));
+
+/** Blocks */
+$modversion['blocks'][1] = array(
+	'file' => 'list_cms.php',
+	'name' => _MI_CMS_LIST,
+	'description' => _MI_CMS_LISTDSC,
+	'show_func' => 'show_list_cms',
+	'edit_func' => 'edit_list_cms',
+	'options' => '5|0|0|0',
+	'template' => 'cms_block_list.html'
+);
+
+$modversion['blocks'][2] = array(
+	'file' => 'random_cms.php',
+	'name' => _MI_CMS_RANDOM,
+	'description' => _MI_CMS_RANDOMDSC,
+	'show_func' => 'show_random_cms',
+	'edit_func' => 'edit_random_cms',
+	'options' => '5|1|1|0',
+	'template' => 'cms_block_random.html'
+);
+
+$modversion['blocks'][3] = array(
+	'file' => 'slider_cms.php',
+	'name' => _MI_CMS_SLIDER,
+	'description' => _MI_CMS_SLIDERDSC,
+	'show_func' => 'show_slider_cms',
+	'edit_func' => 'edit_slider_cms',
+	'options' => '3|0|1|0',
+	'template' => 'cms_block_slider.html'
+);
+
+$modversion['blocks'][4] = array(
+	'file' => 'ticker_cms.php',
+	'name' => _MI_CMS_TICKER,
+	'description' => _MI_CMS_TICKERDSC,
+	'show_func' => 'show_ticker_cms',
+	'edit_func' => 'edit_ticker_cms',
+	'options' => '5|0|0|0',
+	'template' => 'cms_block_ticker.html'
+);
+
+/** Preferences */
+$modversion['config'][1] = array(
+  'name' => 'index_display_mode',
+  'title' => '_MI_CMS_INDEX_DISPLAY_MODE',
+  'description' => '_MI_CMS_INDEX_DISPLAY_MODE_DSC',
+  'formtype' => 'yesno',
+  'valuetype' => 'int',
+  'default' =>  '1');
+
+$modversion['config'][] = array(
+  'name' => 'number_of_cms_per_page',
+  'title' => '_MI_CMS_NUMBER_CMS_PER_PAGE',
+  'description' => '_MI_CMS_NUMBER_CMS_PER_PAGE_DSC',
+  'formtype' => 'textbox',
+  'valuetype' => 'int',
+  'default' =>  '5');
+
+$modversion['config'][] = array(
+	'name' => 'show_breadcrumb',
+	'title' => '_MI_CMS_SHOW_BREADCRUMB',
+	'description' => '_MI_CMS_SHOW_BREADCRUMB_DSC',
+	'formtype' => 'yesno',
+	'valuetype' => 'int',
+	'default' => '0');
+
+$modversion['config'][] = array(
+	'name' => 'show_view_counter',
+	'title' => '_MI_CMS_SHOW_VIEW_COUNTER',
+	'description' => '_MI_CMS_SHOW_VIEW_COUNTER_DSC',
+	'formtype' => 'yesno',
+	'valuetype' => 'int',
+	'default' => '0');
+
+$modversion['config'][] = array(
+	'name' => 'show_tag_select_box',
+	'title' => '_MI_CMS_SHOW_TAG_SELECT_BOX',
+	'description' => '_MI_CMS_SHOW_TAG_SELECT_BOX_DSC',
+	'formtype' => 'yesno',
+	'valuetype' => 'int',
+	'default' => '0');
+
+$modversion['config'][] = array(
+	'name' => 'display_start_logos',
+	'title' => '_MI_CMS_DISPLAY_START_LOGOS',
+	'description' => '_MI_CMS_DISPLAY_START_LOGOS_DSC',
+	'formtype' => 'yesno',
+	'valuetype' => 'int',
+	'default' => '1');
+
+$modversion['config'][] = array(
+	'name' => 'start_logo_position',
+	'title' => '_MI_CMS_START_LOGO_POSITION',
+	'description' => '_MI_CMS_START_LOGO_POSITION_DSC',
+	'formtype' => 'select',
+	'valuetype' => 'int',
+	'options' => array('_MI_CMS_LEFT' => 0, '_MI_CMS_RIGHT' => 1),
+	'default' => 0);
+
+$modversion['config'][] = array(
+	'name' => 'freestyle_logo_dimensions',
+	'title' => '_MI_CMS_FREESTYLE_LOGO_DIMENSIONS',
+	'description' => '_MI_CMS_FREESTYLE_LOGO_DIMENSIONS_DSC',
+	'formtype' => 'yesno',
+	'valuetype' => 'int',
+	'default' => '0');
+
+$modversion['config'][] = array(
+  'name' => 'logo_index_display_width',
+  'title' => '_MI_CMS_LOGO_INDEX_DISPLAY_WIDTH',
+  'description' => '_MI_CMS_LOGO_INDEX_DISPLAY_WIDTH_DSC',
+  'formtype' => 'text',
+  'valuetype' => 'int',
+  'default' =>  '150');
+
+$modversion['config'][] = array(
+  'name' => 'logo_single_display_width',
+  'title' => '_MI_CMS_LOGO_SINGLE_DISPLAY_WIDTH',
+  'description' => '_MI_CMS_LOGO_SINGLE_DISPLAY_WIDTH_DSC',
+  'formtype' => 'text',
+  'valuetype' => 'int',
+  'default' =>  '300');
+
+$modversion['config'][] = array(
+  'name' => 'logo_block_display_width',
+  'title' => '_MI_CMS_LOGO_BLOCK_DISPLAY_WIDTH',
+  'description' => '_MI_CMS_LOGO_BLOCK_DISPLAY_WIDTH_DSC',
+  'formtype' => 'text',
+  'valuetype' => 'int',
+  'default' =>  '100');
+
+$modversion['config'][] = array(
+	'name' => 'logo_upload_height',
+	'title' => '_MI_CMS_LOGO_UPLOAD_HEIGHT',
+	'description' => '_MI_CMS_LOGO_UPLOAD_HEIGHT_DSC',
+	'formtype' => 'textbox',
+	'valuetype' => 'int',
+	'default' =>  '500');
+
+$modversion['config'][] = array(
+	'name' => 'logo_upload_width',
+	'title' => '_MI_CMS_LOGO_UPLOAD_WIDTH',
+	'description' => '_MI_CMS_LOGO_UPLOAD_WIDTH_DSC',
+	'formtype' => 'textbox',
+	'valuetype' => 'int',
+	'default' =>  '500');
+
+$modversion['config'][] = array(
+	'name' => 'logo_file_size',
+	'title' => '_MI_CMS_LOGO_FILE_SIZE',
+	'description' => '_MI_CMS_LOGO_FILE_SIZE_DSC',
+	'formtype' => 'textbox',
+	'valuetype' => 'int',
+	'default' =>  '2097152'); // 2MB default max upload size
+
+$modversion['config'][] = array(
+	'name' => 'show_last_updated',
+	'title' => '_MI_CMS_SHOW_LAST_UPDATED',
+	'description' => '_MI_CMS_SHOW_LAST_UPDATED_DSC',
+	'formtype' => 'yesno',
+	'valuetype' => 'int',
+	'default' => '1');
+
+$modversion['config'][] = array(
+  'name' => 'updated_notice_period',
+  'title' => '_MI_CMS_UPDATED_NOTICE_PERIOD',
+  'description' => '_MI_CMS_UPDATED_NOTICE_PERIOD_DSC',
+  'formtype' => 'select',
+  'valuetype' => 'int',
+  'options' => array(
+	  '_MI_CMS_ONE_DAY' => 1,
+	  '_MI_CMS_THREE_DAYS' => 2,
+	  '_MI_CMS_ONE_WEEK' => 3,
+	  '_MI_CMS_TWO_WEEKS' => 4,
+	  '_MI_CMS_THREE_WEEKS' => 5,
+	  '_MI_CMS_FOUR_WEEKS' => 6),
+  'default' => 3);
+
+$modversion['config'][] = array(
+  'name' => 'date_format',
+  'title' => '_MI_CMS_DATE_FORMAT',
+  'description' => '_MI_CMS_DATE_FORMAT_DSC',
+  'formtype' => 'textbox',
+  'valuetype' => 'text',
+  'default' =>  'j/n/Y');
