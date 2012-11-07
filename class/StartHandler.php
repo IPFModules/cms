2f1f7b5e30c45d48a8324aa75db44c6d30861605
René Sato <sato-san@impresscms.org>
2012-11-07 09:11:14 +0000

    Bugfix for global search.
    git-svn-id: https://subversion.assembla.com/svn/impresscmsaddons/modules/cms/trunk@24696 7a4d4d67-0c3a-0410-b24c-89d50c169223

--- class/StartHandler.php
+++ class/StartHandler.php
@@ -129,7 +129,7 @@ class mod_cms_StartHandler extends icms_ipf_Handler
 
 		if ($userid != 0) 
 		{
-			$criteria->add(new icms_db_criteria_Item('submitter', $userid));
+			$criteria->add(new icms_db_criteria_Item('creator', $userid));
 		}
 		
 		if ($queryarray) 
