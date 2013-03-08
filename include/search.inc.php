f74da64db5bed682b3d23ce1faa7171a71a1f0f3
Simon <simon@isengard.biz>
2013-03-08 02:55:57 +0000

    Improve speed/efficiency of search function.
    git-svn-id: https://subversion.assembla.com/svn/impresscmsaddons/modules/cms/trunk@24931 7a4d4d67-0c3a-0410-b24c-89d50c169223

--- include/search.inc.php
+++ include/search.inc.php
@@ -16,20 +16,45 @@ defined("ICMS_ROOT_PATH") or die("ICMS root path not defined");
 
 function cms_search($queryarray, $andor, $limit, $offset, $userid)
 {
+	global $icmsConfigSearch;
+	
+	$startArray = $ret = array();
+	$count = $number_to_process = $starts_left = '';
+	
 	$cms_start_handler = icms_getModuleHandler("start", basename(dirname(dirname(__FILE__))), "cms");
 	$startArray = $cms_start_handler->getCmsForSearch($queryarray, $andor, $limit, $offset, $userid);
-	$ret = array();
+	
+	// Count the number of records
+	$count = count($startArray);
+	
+	// The number of records actually containing start objects is <= $limit, the rest are padding
+	$starts_left = ($count - ($offset + $icmsConfigSearch['search_per_page']));
+	if ($starts_left < 0) {
+		$number_to_process = $icmsConfigSearch['search_per_page'] + $starts_left; // $starts_left is negative
+	} else {
+		$number_to_process = $icmsConfigSearch['search_per_page'];
+	}
 
-	foreach ($startArray as $start) 
+	// Process the actual starts (not the padding)
+	for ($i = 0; $i < $number_to_process; $i++)
 	{
 		$item['image'] = "images/start.png";
-		$item['link'] = $start->getItemLink(TRUE) . '&amp;seite=' . $start->short_url();
-		$item['title'] = $start->getVar("title");
-		$item['time'] = $start->getVar("date", "e");
-		$item['uid'] = $start->getVar("creator");
+		$item['link'] = $startArray[$i]->getItemLink(TRUE) . '&amp;title=' . $startArray[$i]->short_url();
+		$item['title'] = $startArray[$i]->getVar("title");
+		$item['time'] = $startArray[$i]->getVar("date", "e");
+		$item['uid'] = $startArray[$i]->getVar("creator");
 		$ret[] = $item;
 		unset($item);
 	}
+
+	// Restore the padding (required for 'hits' information and pagination controls). The offset
+	// must be padded to the left of the results, and the remainder to the right or else the search
+	// pagination controls will display the wrong results (which will all be empty).
+	// Left padding = -($limit + $offset)
+	$ret = array_pad($ret, -($offset + $number_to_process), 1);
+	
+	// Right padding = $count
+	$ret = array_pad($ret, $count, 1);
 
 	return $ret;
 }
